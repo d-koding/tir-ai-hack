46cec82dc459d68b024cdc31b67f3f917353e03f
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2025-04-05 14:34:55 -0700

    sidebar

--- tir-ai/app/layout.tsx
+++ tir-ai/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/ui/Footer";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
         {children}
+      </main>
+
+      </SidebarProvider>
+
       </body>
     </html>
   );
